931b7799b3a9bd077bf58df9947e622ad66dd959
Gracie Richards <gracier@gmail.com>
2024-03-13 19:18:24 -0400

    not much added

--- assignment4/src/Controller.java
+++ assignment4/src/Controller.java
@@ -2,19 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Maybe the create portfolio command for the user can be in the format:
+ * These are the possible commands the user can provide when using this program.
  * create portfolio <portfolio_name> MSFT:20, AAPL:10, NVDA:30
  * for a user who wants to make a new portfolio with 20 shares of MSFT, 10 shares of AAPL, and 30
  * shares of NVDA
- * The load portfolio from file command can be:
+ * The load portfolio from file command is:
  * load portfolio <portfolio_name> <filename>
- * The save portfolio to file command can be:
+ * The save portfolio to file command is:
  * save <portfolio_name> <filename>
- * The command to examine the composition of a portfolio can be:
+ * The command to examine the composition of a portfolio is:
  * list <portfolio_name>
- * The command to determine the total value of a portfolio on a given date can be:
+ * The command to determine the total value of a portfolio on a given date is:
  * value <portfolio_name> MM/DD/YYYY
- * There can be an optional command for the user to look up ticker symbols that match the name of a
+ * There is an optional command for the user to look up ticker symbols that match the name of a
  * company, or all ticker symbols that start with the inputted string. It is in the format:
  * search <company_name>
  * OR
