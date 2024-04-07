d77a218576497919678aab627b66ba9764532039
Gracie Richards <gracier@gmail.com>
2024-04-06 20:43:32 -0400

    Added display of current portfolios

--- assignment4/src/GUIView.java
+++ assignment4/src/GUIView.java
@@ -2,6 +2,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 /**
  * An implementation of GUIViewInterface. An object of this class should be used for all GUI
@@ -11,7 +13,9 @@ public class GUIView extends JFrame implements GUIViewInterface {
   //Drop down menu of options for commands
   private JLabel comboboxDisplay;
   JComboBox<String> combobox;
-  //private JPanel mainPanel;
+  //private JLabel portfolioMenuDisplay;
+  private JPanel mainPanel;
+  private JList<String> portfoliosInMenu;
   public GUIView(String caption) {
     super(caption);
     setSize(750, 500);
@@ -20,10 +24,29 @@ public class GUIView extends JFrame implements GUIViewInterface {
 
     this.setLayout(new FlowLayout());
 
-    //mainPanel = new JPanel();
+    mainPanel = new JPanel();
+    mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
 
+    makeMainMenu();
+    makePortfolioMenu();
+
+    setContentPane(mainPanel);
+    //pack();
+    setVisible(true);
+  }
+
+  @Override
+  public void addFeatures(Features features) {
+    /*combobox.addActionListener(new AbstractAction() {
+      public void actionPerformed(ActionEvent e) {
+        JComboBox<String> cb = (JComboBox<String>)e.getSource();
+        String selectedItem = (String)cb.getSelectedItem();
+      }
+    });*/
+  }
+
+  private void makeMainMenu() {
     JPanel comboboxPanel = new JPanel();
-    //mainPanel.add(comboboxPanel);
     comboboxDisplay = new JLabel("Please select one of the following actions from the menu.");
     comboboxPanel.add(comboboxDisplay);
     String[] options = {"---", "Create a new portfolio", "Load portfolio from file",
@@ -43,20 +66,35 @@ public class GUIView extends JFrame implements GUIViewInterface {
       combobox.addItem(options[i]);
     }
     comboboxPanel.add(combobox);
-    setContentPane(comboboxPanel);
-
-    //pack();
-    setVisible(true);
+    //setContentPane(comboboxPanel);
+    mainPanel.add(comboboxPanel);
   }
 
-  @Override
-  public void addFeatures(Features features) {
-    /*combobox.addActionListener(new AbstractAction() {
-      public void actionPerformed(ActionEvent e) {
-        JComboBox<String> cb = (JComboBox<String>)e.getSource();
-        String selectedItem = (String)cb.getSelectedItem();
+  private void makePortfolioMenu() {
+    JPanel selectionListPanel = new JPanel();
+    //portfolioMenuDisplay = new JLabel("User Portfolios");
+    //selectionListPanel.add(portfolioMenuDisplay);
+    selectionListPanel.setBorder(BorderFactory.createTitledBorder("User Portfolios"));
+    //selectionListPanel.setAlignmentX(LEFT_ALIGNMENT);
+    //selectionListPanel.setLayout(new BoxLayout(selectionListPanel, BoxLayout.X_AXIS));
+    //selectionListPanel.setPreferredSize(new Dimension(250, 500));
+
+    DefaultListModel<String> dataForPortfoliosInMenu = new DefaultListModel<>();
+    dataForPortfoliosInMenu.addElement("GR Portfolio");
+    dataForPortfoliosInMenu.addElement("TG Portfolio");
+    dataForPortfoliosInMenu.addElement("Portfolio3");
+    portfoliosInMenu = new JList<>(dataForPortfoliosInMenu);
+    portfoliosInMenu.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+    portfoliosInMenu.addListSelectionListener(new ListSelectionListener() {
+      @Override
+      public void valueChanged(ListSelectionEvent e) {
+        //Use portfoliosInMenu.getSelectedValue() which is the selected String
       }
-    });*/
+    });
+    portfoliosInMenu.setFont(new Font("Arial",Font.PLAIN,16));
+    selectionListPanel.add(portfoliosInMenu);
+    //setContentPane(selectionListPanel);
+    mainPanel.add(selectionListPanel);
   }
 
   @Override
@@ -66,7 +104,7 @@ public class GUIView extends JFrame implements GUIViewInterface {
 
   @Override
   public void showTickerMatches(String csvContents) {
-
+    /**This functionality is not required for the GUI.**/
   }
 
   @Override
@@ -91,6 +129,6 @@ public class GUIView extends JFrame implements GUIViewInterface {
 
   @Override
   public void printMenu() {
-
+    /**This functionality is not required for the GUI.**/
   }
 }
