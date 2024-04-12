19609a98c125da0efd9cb02c1141702167a6d351
Gracie Richards <gracier@gmail.com>
2024-04-11 20:42:18 -0400

    Changed frequency string text box

--- assignment4/src/CommandDollarCost.java
+++ assignment4/src/CommandDollarCost.java
@@ -38,7 +38,8 @@ public class CommandDollarCost implements Command {
     dollarCostPane.add(view.dollarCostEndDateTextBox);
 
     view.frequencyStringTextBox = new JTextArea(1, 50);
-    view.frequencyStringTextBox.setBorder(BorderFactory.createTitledBorder("Frequency"));
+    view.frequencyStringTextBox.setBorder(BorderFactory.createTitledBorder("Frequency (in number of"
+            + " days)"));
     dollarCostPane.add(view.frequencyStringTextBox);
 
     JLabel text2 = new JLabel("Enter the desired stocks and their weights in the following "
